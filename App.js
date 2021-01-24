0e33ae6c946704c00ac969f1fdfca7e9514baedf
Ahra <choar816@postech.ac.kr>
2021-01-24 23:41:32 +0900

    #1.4

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Alert} from "react-native";
 import Loading from "./Loading";
+import * as Location from 'expo-location';
+import axios from "axios";
 
-export default function App() {
-  return <Loading />;
+const API_KEY = "3beac55f4efbb9637af601e81255ebec";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async(latitude, longitude) => {
+    const {data} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  }
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      // Send to API and get weather
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading: false});
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading} = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
